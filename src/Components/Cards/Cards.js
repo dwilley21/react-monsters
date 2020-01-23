00d72973725c3548fc5152a411d5b8fd3872016a
dwilley21 <daltonwilley@gmail.com>
2020-01-22 21:12:50 -0600

    email and sizing added

--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -3,8 +3,9 @@ import React from 'react';
 import './Cards.styles.css'
 export const Cards = (props) => (
     <div className="card-container">
-        <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set2`} />
+        <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} />
         <h1> {props.monster.name}</h1>
+        <h2> {props.monster.email}</h2>
     </div>
 )
 
